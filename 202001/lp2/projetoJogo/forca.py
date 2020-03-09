968731ff3a7ac1876d073a47c8a960f4b719410a
Joao Paulo Dantas <joaod3v@gmail.com>
2020-03-09 14:52:43 -0300

    resolucao novos exercicios URI

--- 202001/lp2/projetoJogo/forca.py
+++ 202001/lp2/projetoJogo/forca.py
@@ -3,6 +3,22 @@ def jogar_forca():
     print("Bem vindo ao jogo de forca!")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    #enquanto nao enforcou (false) e nao acertou (false)
+    while(not enforcou and not acertou):
+        
+        chute = input("Qual a letra? ")
+        #palavra_secretea.find(chute)
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posicao {}".format(letra,index))
+            index += 1
+        print("jogando...")
 
     print("fim do jogo")
 
