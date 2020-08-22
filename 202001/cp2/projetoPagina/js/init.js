6d090abddd87a17ec99c8dc1c6b7e09cdd85f251
Joao Paulo Dantas <joaod3v@gmail.com>
2020-08-21 21:12:01 -0300

    criacao projeto de paginas para CP2

---
+++ 202001/cp2/projetoPagina/js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.slider').slider();
+    $('.carousel').carousel();
+    window.setInterval(function(){$('.carousel').carousel('next')},5000);
+    $('.collapsible').collapsible();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
