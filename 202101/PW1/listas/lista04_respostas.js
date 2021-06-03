dbd4fe9783b989093c9a67c3feafaa0f1cf17584
Joao Paulo Dantas <joaod3v@gmail.com>
2021-06-03 10:07:25 -0300

    finalizacao lista 04 js

--- 202101/PW1/listas/lista04_respostas.js
+++ 202101/PW1/listas/lista04_respostas.js
@@ -81,3 +81,127 @@ function somaArrays(arr1,arr2){
 console.log(  somaArrays(a,b) );
 
 console.log('\n*************************** RESPOSTA QUESTAO 7 **************************************')
+/*
+function ehPalindromo(str) {     
+    for(var i = 0; i < str.length / 2; i++) {
+        if (str[i] != str[str.length - i - 1]) {
+            return false;
+        }
+            
+    return true;
+    }
+        
+} */
+
+function ehPalindromo(str){
+    let str_inversa = str.split("").reverse().join("");
+    return str == str_inversa;
+}
+console.log(ehPalindromo("radar"));
+console.log(ehPalindromo("reviver"));
+console.log(ehPalindromo("osso"));
+console.log(ehPalindromo("palindromo"));
+
+console.log('\n*************************** RESPOSTA QUESTAO 8 **************************************')
+
+function inverte(str){
+    return str.split("").reverse().join("");
+}
+
+console.log( inverte("Abacaxi") );
+
+
+console.log('\n*************************** RESPOSTA QUESTAO 9 **************************************')
+
+let familias = [
+    ['Fred','Barney'],
+    ['George','Jane','Elroy'],
+    ['Homer','Marge','Barney'],
+]
+
+for (let familia of familias){
+    let linha = "";
+    for( let membro of familia){
+        linha = linha + membro + '\t' ;   
+    }
+    console.log(linha)
+}
+
+console.log('\n*************************** RESPOSTA QUESTAO 10 **************************************')
+let usuarios = [
+    {
+        nome:'Rodrigo Melo Fernandes',
+        email:'rodrigo1999@zzzmail.com',
+        categorias: ['premium', 'admin', 'proprietario'],
+    },
+    {
+        nome:'Letícia Goncalves Azevedo',
+        email:'lga@dayrep.com',
+        categorias: ['admin'],
+    },
+    {
+        nome:'Pedro Almeida Oliveira',
+        email:'poliv3@armyspy.com',
+        categorias: ['premium', 'promocao'],
+    }
+];
+
+for(let usuario of usuarios){
+    for(let item in usuario){
+        console.log(`${item}:${usuario[item]}`)
+    }
+    console.log('---')
+}
+
+console.log('\n*************************** RESPOSTA QUESTAO 11 **************************************')
+let aa = [ 
+            [4,5,6],
+            [10,20,30] 
+        ];
+let bb = [ 
+            [1,2,3],
+            [5,8,20] 
+        ];
+/*
+function somaMatrizes(arr1,arr2){
+    let arr= [];
+    if(arr1.length === arr2.length){
+
+        for(let j in arr1){
+            arr.push( arr1[j] + arr2[j] );
+        }
+
+        return arr;
+    }
+    console.log('Os arrays devem ter o mesmo tamanho')
+}*/
+
+function somaMatrizes(arr1,arr2){
+    let arr =[];
+    let arrElem = [];
+    let arrSum = [];
+
+    let len = arr1.length;
+    
+    for(let i=0; i<len; i++){
+
+        arrElem=[];
+        for(let j of arr1[i]){
+            
+            arrElem.push(j);
+        }
+
+        arrSum = [];
+        for(let k in arr2[i]){
+
+          arrSum.push(arrElem[k] + arr2[i][k]) ;
+        }
+
+        arr.push(arrSum);
+        
+    }
+
+    console.log(arr)
+}
+
+console.log(  somaMatrizes(aa,bb) );
