db608914db46f94eee33ba5ad17e7282e7939564
Joao Paulo Dantas <joaod3v@gmail.com>
2021-08-17 19:14:11 -0300

    eliminacao de arquivos desnecessario

--- 202101/PW1/projeto_final/src/view/cadastro.js
+++ 202101/PW1/projeto_final/src/view/cadastro.js
@@ -1,82 +1,187 @@
-let selectedRow = null
+$(document).ready(function(){
+    
+    getUsuarios();
 
-function onFormSubmit() {
-    if (validate()) {
-        let formData = readFormData();
+    
+    $('#atualizarBtn').hide()
 
-        console.log(form);
+    function getUsuarios(){
+        $("#listausuarios").html('');
+        $.ajax({
+            url: 'http://localhost:3000/usuarios',
+            method: 'get',
+            dataType: 'json',
+            success: function(data) {
+                $(data).each(function(i, usuario){
+                    $('#listausuarios').append($("<tr>")
+                                            .append($("<td class='1nome'>").append(usuario.primeiro_nome))
+                                            .append($("<td class='2nome'>").append(usuario.ultimo_nome))
+                                            .append($("<td class='email'>").append(usuario.email))
+                                            .append($("<td class='telefone'>").append(usuario.telefone))
+                                            .append($("<td>").append(usuario.id))
+                                            .append($("<td>").append("<button id=btn_edit value="+usuario.id+">Editar"))
+                                            .append($("<td>").append("<button id=btn_apag value="+usuario.id+">Apagar"))
+                                            );
+                    });
+                }
+        });
+    };
+    
+    $(document).on("click", "#btn_edit", function(e) {
+        e.preventDefault();
 
-        if (selectedRow == null)
-            insertNewRecord(formData);
-        else
-            updateRecord(formData);
-        resetForm();
+        $('#atualizarBtn').show()
+        $('#cadastrarBtn').hide()
+
+        let id = $(this).val();
+
+        //CAPTURA DADOS DA LINHA DA TABELA
+        let row = $(this).closest('tr');
+        
+        let nome1 = row.find('.1nome').text();
+        let nome2 = row.find('.2nome').text();
+        let email = row.find('.email').text();
+        let telefone = row.find('.telefone').text();
+
+
+        //ATUALIZA DADOS DO FORMULARIO
+        $('#nome1').val( nome1 );
+        $('#nome2').val( nome2 );
+        $('#email').val( email );
+        $('#telefone').val( telefone );
+
+
+        //ACOMPANHA ALTERACOES NOS CAMPOS DO FORMULARIO 
+        $("#nome1").change(function(){
+            nome1 = $(this).val();
+            console.log(nome1);
+        });
+
+        $("#nome2").change(function(){
+            nome2 = $(this).val();
+            console.log(nome2);
+        });
+
+        $("#email").change(function(){
+            email = $(this).val();
+            console.log(email);
+        });
+
+        $("#telefone").change(function(){
+            telefone = $(this).val();
+            console.log(telefone);
+        });
+
+       
+        $("#atualizarBtn").click(function(){
+            let data = {
+                primeiro_nome: nome1,
+                ultimo_nome: nome2,
+                email: email,
+                telefone: telefone
+            } 
+            console.log(data);
+
+            submterUsuario( id, data, "PUT");
+
+            limparFomulario();
+
+            getUsuarios();
+
+            $('#atualizarBtn').show()
+            $('#cadastrarBtn').hide()
+        });
+       
+     });
+
+    $("#cadastrarBtn").click(function(e){
+
+        e.preventDefault();
+
+        let data = {
+            primeiro_nome: $('#nome1').val(),
+            ultimo_nome: $('#nome2').val(),
+            email: $('#email').val(),
+            telefone: $('#telefone').val()
+        } 
+        console.log(data);
+
+        submterUsuario( 0, data, "POST");
+
+        limparFomulario();
+
+        getUsuarios();
+    });
+    
+    $(document).on("click","#btn_apag",function(e){
+
+        e.preventDefault();
+
+        let id = $(this).val();
+
+        console.log(id);
+
+        let data = {};
+
+        submterUsuario( id, data, "DELETE");
+           
+        getUsuarios();
+    });
+
+
+         
+    function submterUsuario(id, data, metodo){
+
+        if("PUT" == metodo){
+            $.ajax({
+                url: 'http://localhost:3000/usuarios/' + id,
+                method: 'PUT',
+                dataType: 'text',
+                contentType: 'application/json',
+                data: JSON.stringify(data),
+                success: function(data) {
+                    alert(data);
+                }
+            });
+
+        }else if(metodo == "POST"){
+
+            console.log(data);
+
+            $.ajax({
+                url: 'http://localhost:3000/usuarios/',
+                method: 'POST',
+                dataType: 'text',
+                contentType: 'application/json',
+                data: JSON.stringify(data),
+                success: function(data) {
+                    alert(data);
+                }
+            });
+        }else if(metodo == "DELETE"){
+            $.ajax({
+                url: 'http://localhost:3000/usuarios/' + id,
+                method: 'DELETE',
+                dataType: 'text',
+                success: function(data) {
+                    alert(data);
+                }
+            });
+
+        }else{}
+        
     }
-}
 
-function readFormData() {
-    let formData = {};
-    formData["fullName"] = document.getElementById("fullName").value;
-    formData["empCode"] = document.getElementById("empCode").value;
-    formData["salary"] = document.getElementById("salary").value;
-    formData["city"] = document.getElementById("city").value;
-    return formData;
-}
-
-function insertNewRecord(data) {
-    let table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
-    let newRow = table.insertRow(table.length);
-    cell1 = newRow.insertCell(0);
-    cell1.innerHTML = data.fullName;
-    cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.empCode;
-    cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.salary;
-    cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.city;
-    cell4 = newRow.insertCell(4);
-    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
-                       <a onClick="onDelete(this)">Delete</a>`;
-}
-
-function resetForm() {
-    document.getElementById("fullName").value = "";
-    document.getElementById("empCode").value = "";
-    document.getElementById("salary").value = "";
-    document.getElementById("city").value = "";
-    selectedRow = null;
-}
-
-function onEdit(td) {
-    selectedRow = td.parentElement.parentElement;
-    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("empCode").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("city").value = selectedRow.cells[3].innerHTML;
-}
-function updateRecord(formData) {
-    selectedRow.cells[0].innerHTML = formData.fullName;
-    selectedRow.cells[1].innerHTML = formData.empCode;
-    selectedRow.cells[2].innerHTML = formData.salary;
-    selectedRow.cells[3].innerHTML = formData.city;
-}
-
-function onDelete(td) {
-    if (confirm('Are you sure to delete this record ?')) {
-        row = td.parentElement.parentElement;
-        document.getElementById("employeeList").deleteRow(row.rowIndex);
-        resetForm();
+    function limparFomulario(){
+        $('#nome1').val('');
+        $('#nome2').val('');
+        $('#email').val('');
+        $('#telefone').val('');
     }
-}
-function validate() {
-    isValid = true;
-    if (document.getElementById("fullName").value == "") {
-        isValid = false;
-        document.getElementById("fullNameValidationError").classList.remove("hide");
-    } else {
-        isValid = true;
-        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
-            document.getElementById("fullNameValidationError").classList.add("hide");
-    }
-    return isValid;
-}
+    
+});
+  
+
+
+
+
